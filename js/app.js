b84bd2aadd68a22d8dc545af84517b19564175d3
Ahmed <w33zy.7@gmail.com>
2017-11-13 20:36:41 +0200

    Final Release

--- js/app.js
+++ js/app.js
@@ -1,65 +1,101 @@
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 function CardList() {
 	this.deck = $('.deck');
 	this.cards = $('.card');
+	this.moves = 0;
+	this.matched = 0;
+	this.rate = 5;
+	this.shuffle();
 }
 CardList.prototype = {
+	restart: function () {
+		this.shuffle();
+		this.closeAll();
+		$('.moves').text(0);
+
+		//reset stars
+		this.rate = 5;
+		let star = $('.stars').find('.fa');
+		star.addClass('fa-star');
+		star.removeClass('fa-star-o');
+	},
 	shuffle: function () {
 		let randomIndex, tempCard;
 
+		//shuffle cards
 		for (let i = 0; i < this.cards.length; i++) {
 			randomIndex = Math.floor(Math.random() * i);
 			tempCard = this.cards[i];
 			this.cards[i] = this.cards[randomIndex];
 			this.cards[randomIndex] = tempCard;
 		}
+
+		//empty the old deck and add the new cards
 		this.deck.empty();
 		this.deck.append(this.cards);
+		//the event handling function
+		this.play();
 	},
-	toggleOpen: function (card) {
-		$(card).toggleClass('open show');
+	open: function (card) {
+		$(card).addClass('open show');
+		this.count();
 	},
-	match: function () {
-		let openedList = $('.open');
-		if (openedList.length == 2) {
-			if ($(openedList[0]).children().attr('class') == $(openedList[1]).children().attr('class'))
-			{
-				openedList.addClass('match');
-				this.toggleOpen(openedList);
-			}
-		} else if (openedList.length > 2) {
-			this.toggleOpen(openedList);
+	isOpened(card) {
+		return card.hasClass('open') ? true : false;
+	},
+	close: function (card) {
+		$(card).removeClass('open show wrong');
+	},
+	closeAll: function () {
+		this.cards.removeClass('open show match');
+	},
+	count: function () {
+		$('.moves').text(++this.moves);
+
+		//remove stars every 5 counts
+		let star = $('.stars').find('.fa').last();
+		if (this.moves > this.rate) {
+			star.removeClass('fa-star');
+			star.addClass('fa-star-o');
+			this.rate += 5;
 		}
 	},
-	displayCard: function (i) {
+	isWon: function () {
+		if (this.matched == 8) {
+			this.deck.empty();
+			this.deck.append().html(`<h1> Congratulations you've won in ${this.moves} moves </h1>`);
+		}
+	},
+	dismatch: function (cardList) {
+		cardList.addClass('wrong');
+		setTimeout(this.close, 500, cardList);
+	},
+	match: function () {
+		let openedCards = $('.open');
+		if (openedCards.length == 2) {
+			//check if both have the same hidden symbol
+			if ($(openedCards[0]).children().attr('class') == $(openedCards[1]).children().attr('class')) {
+				openedCards.addClass('match');
+				//remove the matched cards from the openedCards list
+				this.close(openedCards);
+				this.matched++;
+				//check if the user has won yet
+				this.isWon();
+			} else {
+				this.dismatch(openedCards);
+			}
+		}
+	},
+	play: function () {
 		for (let i = 0; i < this.cards.length; i++) {
-			$(this.cards[i]).on('click', (e) => {
+			let card = $(this.cards[i]);
+			card.on('click', (e) => {
 				e.preventDefault();
-				this.toggleOpen($(this.cards[i]));
-				this.match();
+				if (!this.isOpened(card)) {
+					this.open(card);
+					this.match();
+				}
 			});
 		}
 	}
 }
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
-
-var x = new CardList;
-x.shuffle();
-x.displayCard();
+var myDeck = new CardList;
