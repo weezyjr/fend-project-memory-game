44bf7bdb02fbe8ab4efbbe01c6e9fdc33277734d
Ahmed <w33zy.7@gmail.com>
2017-11-13 18:55:07 +0200

    Add open and show functions
    Add function open that opens cards, and function show that shows the symbols

--- js/app.js
+++ js/app.js
@@ -1,36 +1,43 @@
 /*
- * Create a list that holds all of your cards
- */
-
-function CardList() {
-    this.deck = $('.deck');
-    this.cards = $('.deck').children();
-}
-CardList.prototype.shuffle = function () {
-    let randomIndex, tempCard;
-
-    for (let i = 0; i < this.cards.length; i++) {
-        randomIndex = Math.floor(Math.random() * i);
-        tempCard = this.cards[i];
-        this.cards[i] = this.cards[randomIndex];
-        this.cards[randomIndex] = tempCard;
-    }
-
-    this.deck.empty();
-    this.deck.append(this.cards);
-    return this.deck;
-}
-
-var x = new CardList;
-x.shuffle();
-
-/*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
  *   - loop through each card and create its HTML
  *   - add each card's HTML to the page
  */
 
+function CardList() {
+	this.deck = $('.deck');
+	this.cards = $('.card');
+}
+CardList.prototype = {
+	shuffle: function () {
+		let randomIndex, tempCard;
+
+		for (let i = 0; i < this.cards.length; i++) {
+			randomIndex = Math.floor(Math.random() * i);
+			tempCard = this.cards[i];
+			this.cards[i] = this.cards[randomIndex];
+			this.cards[randomIndex] = tempCard;
+		}
+		this.deck.empty();
+		this.deck.append(this.cards);
+	},
+	open: function (i) {
+		$(this.cards[i]).toggleClass('open');
+	},
+	show: function (i) {
+		$(this.cards[i]).toggleClass('show');
+	},
+	displayCard: function (i) {
+		for (let i = 0; i < this.cards.length; i++) {
+			$(this.cards[i]).on('click', (e) => {
+				e.preventDefault();
+				this.open(i);
+				this.show(i);
+			});
+		}
+	}
+}
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
@@ -41,3 +48,9 @@ x.shuffle();
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+
+
+
+var x = new CardList;
+x.shuffle();
+x.displayCard();
